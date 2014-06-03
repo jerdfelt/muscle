a89f49b2c184a302412a3bac9341837ed5427005
Johannes Erdfelt <johannes@erdfelt.com>
2014-06-03 16:55:06 -0700

    Remove data migration code for now

--- muscle/__init__.py
+++ muscle/__init__.py
@@ -1,10 +1,4 @@
 from sqlalchemy import *
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
-from sqlalchemy.orm import mapper, object_mapper
-
-
-_migrations = []
 
 
 def expand(engine, desired):
@@ -110,60 +104,3 @@ def contract(engine, desired):
         current.reflect(engine)
 
     return current
-
-
-# Simple helper
-def dictify(row):
-    return dict((k, getattr(row, k))
-                for k in object_mapper(row).columns.keys())
-
-
-def _load_listener(target, context):
-    # FIXME: This could be optimized to create a list of only the data
-    # migrations that still need to be run by inspecting the schema on
-    # load
-    for table, srccol, dstcol, func in _migrations:
-        if target.__table__.name != table:
-            continue
-
-        table = target.__table__
-
-        if hasattr(table.c, srccol.name) and hasattr(table.c, dstcol.name):
-            # FIXME: Check schema of srccol and dstcol and make sure they
-            # match
-            func(context.session, table, target)
-
-
-def data_migration(table, srccol, dstcol):
-    """Add data migration function"""
-    def inner(func):
-        _migrations.append((table, srccol, dstcol, func))
-        return func
-
-    return inner
-
-
-# FIXME: It's probably not necessary to explicitly call this patch()
-# func. We could do the same incrementally in data_migration() above.
-def patch(current, desired):
-    for name, dtable in desired.iteritems():
-        if not hasattr(dtable, '__table__'):
-            continue
-
-        tablename = dtable.__table__.name
-
-        if tablename not in current.tables:
-            continue
-
-        ctable = current.tables[tablename]
-
-        ccolumns = dict((c.name, c) for c in ctable.columns)
-        dcolumns = dict((c.name, c) for c in dtable.metadata.tables[tablename].columns)
-
-        # Patch desired schema with rows not deleted yet
-        missing_columns = set(ccolumns.keys()) - set(dcolumns.keys())
-        for name in missing_columns:
-            setattr(dtable, name, ccolumns[name].copy())
-
-        # Setup load event for this class
-        event.listen(dtable, 'load', _load_listener)
