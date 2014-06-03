90796d7b2499eda10e016da3d87fc01be2d68c35
Johannes Erdfelt <johannes@erdfelt.com>
2014-06-03 16:53:15 -0700

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(name='muscle',
+      version='0.0.1',
+      author='Johannes Erdfelt',
+      author_email='johannes@erdfelt.com',
+      url='https://github.com/jerdfelt/muscle',
+      description='Expand/Contract style schema migrations for SQLAlchemy',
+      packages=['muscle'])
